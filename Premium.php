8aa42936543cae42cfb068886cb0f3d32784350e
Evelin <73557609+evelinhrvt@users.noreply.github.com>
2023-03-29 21:10:40 +0200

    jojo

--- Premium.php
+++ Premium.php
@@ -1,64 +1,84 @@
+<?php
+
+session_start();
+if (empty($_SESSION["loggedUserID"])) {
+    header("location: index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Premium</title>
-  <link rel="stylesheet" href="style2.css">
+    <meta charset="UTF-8">
+    <title>Premium</title> <!--adatbazisban lesz-->
+    <link rel="stylesheet" href="css/styleee.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
+    <link rel="icon" type="image/x-icon" href="Kepek/IMDB.png">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <style>
+        .box {
 
-  <style>
-    .box{
+            background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
+            backdrop-filter: blur(10px);
+            -webkit-backdrop-filter: blur(10px);
+            border-radius: 20px;
+            border: 1px solid rgba(255, 255, 255, 0.18);
+            box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
+            display: inline-block;
+            width: 350px;
+            height: 400px;
+            margin: 2px;
+        }
 
-      background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
-      backdrop-filter: blur(10px);
-      -webkit-backdrop-filter: blur(10px);
-      border-radius: 20px;
-      border:1px solid rgba(255, 255, 255, 0.18);
-      box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
-      display: inline-block;
-      width: 350px;
-      height: 400px;
-      margin: 2px;
-    }
+        .kep {
+            display: inline-block;
+            border-radius: 20px;
+            width: 345px;
+            height: 395px;
+            margin: 2px;
 
-    .kep{
-      display: inline-block;
-      border-radius: 20px;
-      width: 345px;
-      height: 395px;
-      margin: 2px;
+        }
 
-    }
-    .kepp{
-      display: inline-block;
-      border-radius: 20px;
-      width: 100px;
-      height: 50px;
-      margin: 5px;
+        .kepp {
+            display: inline-block;
+            border-radius: 20px;
+            width: 100px;
+            height: 50px;
+            margin: 5px;
 
-    }
+        }
 
-  </style>
-
+    </style>
 </head>
 <body>
-<nav>
-  <a href="Home.html">Home</a>
-  <a href="Rules.html">Rules</a>
-  <a class="active" href="Premium.php">Profile</a>
-  <a href="Search.html">Search</a>
-  <div class="animation start-home"></div>
-</nav>
-<div class="box" id="box1">
-  <a href="Sorozat.fooldal.html"><img class="kep" src="Kepek/GoT.jpg"></a></div>
-<!--adatbazisban lesz-->
+<?php
+include 'header.php'; ?>
 
-</div>
-<br>
-Mail: <input type=text name=id value='<?php echo $id ?>' disabled> <br><br>
-Username: <input type=text name=id value='<?php echo $id ?>' disabled> <br><br>
-password: <input type=password name=id value='<?php echo $id ?>' disabled> <br><br>
-<input type="submit">
+<script type="text/javascript">
+    $(document).ready(function () {
+        $.ajax({
+            type: "GET",
+            url: "dbFunctions.php?event=getProfile",
+            cache: false,
+            success: function (res) {
+                res = JSON.parse(res);
+                let i = 0;
+                let data =
+                    '<div class="box" id="box1">' +
+                    '<img class="kep" src="Kepek/' + res[i]['kep'] + '">' +
+                    '</div>' +
+                    '<br><br>' +
+                    'Mail: <input type=text name=id value="' + res[i]['email'] + '" disabled>' +
+                    '<br><br>' +
+                    'Username: <input type=text name=id value="' + res[i]['nev'] + '" disabled>' +
+                    '<br><br>' +
+                    '<h4>password (hash): ' + res[i]['jelszo'] + '</h4>';
 
-</div>
+                $("#profileData").append(data);
+            }
+        });
+    })
+</script>
+<div id="profileData"></div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,56 +3,50 @@
 <head>
     <meta charset="utf-8">
     <title>Login</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="Kepek/IMDB.png">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
 </head>
 <body>
 <div class="center">
     <h1>Login</h1>
-    <form method="post" action="index.php">
+    <form>
         <div class="txt_field">
             <input id="email" type="text" name="email" required>
             <span></span>
             <label>Email</label>
         </div>
         <div class="txt_field">
-            <input id="pass" type="password" name="jelszo" required>
+            <input id="pass" type="password" name="password" required>
             <span></span>
             <label>Password</label>
         </div>
 
-        <input type="submit" value="Login">
-
+        <input type="submit" id="loginBTN" value="Login">
 
         <div class="signup_link">
-            A member? <a href="Registration.html">Signup</a>
+            A member? <a href="registration.php">Signup</a>
         </div>
-
     </form>
 </div>
 
+<script type="text/javascript">
+    $('#loginBTN').click(function () {
+        $.ajax({
+            type: "POST",
+            url: "dbFunctions.php?event=login",
+            cache: false,
+            data: {email: $('#email').val(), password: $('#pass').val()},
+            success: function (res) {
+                if (JSON.parse(res) == 'success') {
+                    window.location = 'home.php';
+                } else {
+                    alert(JSON.parse(res));
+                }
+            }
+        });
+    })
+</script>
 </body>
 </html>
-
-<?php
-$conn = mysqli_connect("localhost", "root", "", "sorinet");
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
-// felhasználó azonosítása
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $password = $_POST["jelszo"];
-
-    $sql = "SELECT id FROM user WHERE email = '$email' AND jelszo = '$password'";
-    $result = mysqli_query($conn, $sql);
-    $count = mysqli_num_rows($result);
-    var_dump($count);
-    // ha a felhasználói adatok helyesek, akkor átirányítunk a főoldalra
-    if ($count >= 1) {
-        header("location: Home.html");
-    } else {
-        echo "Hibás felhasználónév vagy jelszó!";
-    }
-}
-?>
